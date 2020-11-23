f28de1db23877a16ec0d6214420a251f37388493
Anchit Gupta <guptaanchit29@gmail.com>
2020-11-23 21:00:52 +0530

    doxygen comments

--- contracts/AddressTracker.sol
+++ contracts/AddressTracker.sol
@@ -14,9 +14,22 @@ contract AddressTracker {
   address govtAddress;
   bool addressSet;
 
+  /**
+    Constructor for the AddressTracker
+    
+    This function sets initialise addressSet to false
+  */
   constructor() public {
     addressSet = false;
   }
+
+  /**
+    This function sets the government address
+    
+    government address is pushed to authorities array
+    
+    @param _govtAddress    Account to be set as the government address
+  */
 
   function setGovtAddress(address _govtAddress) public {
     require(addressSet == false, "The government address should not be set");
@@ -25,6 +38,10 @@ contract AddressTracker {
     isAuthorized[govtAddress] = true;
     authorities.push(govtAddress);
   }
+
+  /**
+    getter function to get the government address
+  */
   
   function getGovtAddress() public view returns(address) {
     return govtAddress;
@@ -52,6 +69,14 @@ contract AddressTracker {
       return token.balanceOf(person);
   }
   
+  /**
+    This function make an account eligible for minting tokens.
+
+    Only government is allowed to make new issuing authorities.
+    
+    @param _authority    address of account to be made issuing authority.
+  */
+
   function issueAuthorisation(address _authority) public {
     require(msg.sender == govtAddress, "Only government can issue an authorisation");
     isAuthorized[_authority] = true;
@@ -62,9 +87,15 @@ contract AddressTracker {
     return authorities;
   }
 
+  /**
+    This function mints a token for a given location
+    
+    Only issuing authorities are allowed to mint tokens
+    
+    @param locationHash    hash of [longitude, latitude, address_line_1, address_line_2]
+  */
+
   function mintToken(bytes32 locationHash) public {
-    // locationHash -> hash([long, lat, add1, add2])
-    // will get token address
     require(locationExist[locationHash] == false, "Token for this location already exist");
     require(isAuthorized[msg.sender] == true, "Only the government and authorizing organization can mint tokens");
     ERC20AddressTracker tkn = new ERC20AddressTracker("x", "x", locationHash);
@@ -75,6 +106,13 @@ contract AddressTracker {
     tokenOwner[msg.sender].push(tkn);
   }
 
+  /**
+    This function removes a token from the issuing authorities.
+    
+    @param index    index of the token to remove
+    @param array    array from which to remove the token
+  */
+
   function remove(uint index, ERC20AddressTracker[] memory array) private returns(ERC20AddressTracker[] memory) {
     for (uint i = index; i<array.length-1; i++){
         array[i] = array[i+1];
@@ -84,6 +122,14 @@ contract AddressTracker {
     return array;
     }
   
+  /**
+    This function allot a token to the owner of the location.
+    
+    @param locationHash  hash of [longitude, latitude, address_line_1, address_line_2]
+    @param completeHash  hash of [longitude, latitude, address_line_1, address_line_2, all residents, owner]
+    @param owner         Owner of land
+  */
+
   function allot(bytes32 locationHash, bytes32 completeHash, address owner) public {
     // locationHash -> hash([long, lat, add1, add2])
     // completeHash -> hash([long, lat, add1, add2, resident hash, nonce])
@@ -116,6 +162,17 @@ contract AddressTracker {
     tokenOwner[authorities[idx1]] = remove(idx2, tokenOwner[authorities[idx1]]);
     // delete tokenOwner[govtAddress][idx];
   }
+
+  /**
+    This function transfers the ownership of land.
+    
+    The owner can tranfer the ownership by providing correct details
+
+    @param locationHash     hash of [longitude, latitude, address_line_1, address_line_2]
+    @param oldHash          old hash of [longitude, latitude, address_line_1, address_line_2, all residents, owner]
+    @param newHash          new hash of [longitude, latitude, address_line_1, address_line_2, all residents, owner]
+    @param oldOwner         Previous Owner of land
+  */
   
   function transfer(bytes32 locationHash, bytes32 oldHash, bytes32 newHash, address oldOwner) public {
     // locationHash -> hash([long, lat, add1, add2])
